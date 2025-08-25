74059e3706a7f5cfd7525ca56217de0c1b5c25d1
Melpomenna <melpomeNn@yandex.ru>
2025-08-25 14:51:07 +0700

    Change Runtime name, change lniker flags for mimalloc redirect.

--- CMake/CompileOptions.cmake
+++ CMake/CompileOptions.cmake
@@ -6,6 +6,7 @@ function(SetCompileOptions)
                          /NOLOGO
                          /MACHINE:x64
                          /DEBUG:FULL
+                         /INCLUDE:mi_version
         )
         add_compile_options(/MD 
                             /O2
@@ -44,6 +45,7 @@ function(SetCompileOptions)
                          /DEBUG
                          /NOLOGO
                          /MACHINE:x64
+                         /INCLUDE:mi_version
         )
         add_compile_options(/MDd 
                             /arch:AVX2
